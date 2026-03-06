0d6722f24c586c78d1014907237cdd15809f8f20
Shima <116629409+Chang146@users.noreply.github.com>
2026-03-05 17:49:19 -0800

    Add submit page

---
+++ src/components/Submit-Button.tsx
@@ -0,0 +1,10 @@
+
+function SubmitButton() {
+    return (
+        <a href = "" target="_blank" rel="noopener noreferrer">
+        <button className="submit-button">Submit Here</button>
+        </a>
+    ); 
+}
+
+export default SubmitButton;
--- src/pages/Submit.tsx
+++ src/pages/Submit.tsx
@@ -1,12 +1,44 @@
-
+import SubmitButton from "../components/submit-button"
 
 
 export default function Submit(){
 
 
     return <>
-        <div> Submission page is here!</div>
-        <div> Clikc this buttin itsn not a suspicious or danger</div>
-        <button> clikc me</button>
+        <div id="submit-container" style={{
+            display: 'flex', width: '100vw', height:'100vh',
+            flexDirection:'column',
+            alignItems: "center", justifyContent: "center", gap: '1vw',
+            padding:'5vh',
+            backgroundColor: '#25233bff',
+        }}>
+            <div style={{ width: '50%', height: '70%', }}>
+                <h3>Current Theme: "Something"</h3>
+                <hr/> 
+
+                <h3>Info</h3>
+                    <ul>
+                        <li></li>
+                    </ul>
+                <hr/>
+
+                <h3>Submission Guidelines</h3>
+                    <ul>
+                        <li></li>
+                    </ul>
+                <hr/>
+
+                <h3>Deadlines</h3>
+                    <ul>
+                        <li>Submissions open: Date</li>
+                        <li>Submissions close: Date</li>
+                    </ul>
+                <hr/>
+
+            </div>
+            
+            <SubmitButton/>
+
+        </div>
     </>
 }
