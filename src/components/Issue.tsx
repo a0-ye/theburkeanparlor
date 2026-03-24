f0faff6f78ca8f686ee17a7b0af4f3370bd8c0fd
Adrian Y. <acye@ucsd.edu>
2026-03-24 17:09:22 -0500

    issue style update

--- src/components/Issue.tsx
+++ src/components/Issue.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import { Link, useParams } from "react-router-dom"
 import { IssueRegistry } from "../data/IssueRegistry"
+import { colors } from "../assets/colors";
 
 
 export default function Issue() {
@@ -9,40 +10,79 @@ export default function Issue() {
 
     return <>
 
-        <div style={{ display: 'flex', width: '100%' }}>
+        <div id="IssueMain" style={{
+            display: 'flex', flexDirection: 'column',
+            width: '100%', height: '100%',
+            // justifyContent:'center'
+            backgroundColor: colors.light
 
-            <div id="Header" style={{ display: 'flex', padding: '1em' }}>
-                <img
-                    // src={img}
-                    alt="Issue related img here"
-                    style={{
-                        width: 100, height: 100, borderRadius: 100,
-                        padding: '1em'
-                    }}
-                />
 
-                <div>
-                    <div style={{
-                        borderBottom: 'solid 2px black',
-                        fontSize: '5em'
-                    }}> Issue {issueNumber}</div>
-                    <div> HEy! this is a description of the current issue. Put more things here and eventually its gonna be awesome.</div>
+        }}>
+            <div style={{
+                backgroundColor: colors.darkblue
+            }}>
+
+
+                <div id="Header" style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    padding: '1em',
+                    gap: '2em',
+                    backgroundColor: colors.darkblue
+
+                }}>
+                    <img
+                        // src={img}
+                        alt="Issue related img here"
+                        style={{
+                            width: 100, height: 100, borderRadius: 100,
+                            padding: '1em',
+                            backgroundColor: colors.brown
+                        }}
+                    />
+
+                    <div>
+                        <div style={{
+                            fontSize: '5em',
+                            textDecoration: 'underline'
+                        }}> Issue {issueNumber}</div>
+                        <div>
+                            HEy! this is a description of the current issue.
+                            <br />
+                            Put more things here and eventually its gonna be awesome.
+                        </div>
+                    </div>
                 </div>
+
+                <div style={{ borderBottom: 'solid black 2px', margin: ' 3vh 15vw 3vh 15vw' }} />
+
+                <div> Hello put the filter here?</div>
+
+                <div style={{ borderBottom: 'solid black 2px', margin: ' 3vh 15vw 3vh 15vw' }} />
             </div>
 
 
-        </div>
-        <div style={{ display: "grid", gridTemplateColumns: '1fr 1fr 1fr', gap: '2em' }}>
-            {articleData.map((value, idx) => {
-                return <>
-                    <Link to={`/ArticlePage/${value}`}
-                        key={value + idx}
-                        style={{ backgroundColor: '#fff', borderRadius: 15, width: 50, height: 50, color: 'black' }}>
-                        {idx} <br /> {value}
-                    </Link>
-                </>
-            })}
+            <div id="ArticleContainer" style={{
+                margin: '0 10% 0 10%',
+                display: "grid",
+                gridTemplateColumns: '1fr 1fr 1fr',
+                gap: '2em'
+            }}>
+                {articleData.map((value, idx) => {
+                    return <>
+                        <Link to={`/ArticlePage/${value}`}
+                            key={value + idx}
+                            style={{
+                                display: 'flex', alignItems: 'center', justifyContent: 'center',
+                                width: '200px', height: '200px', borderRadius: '100px',
+                                backgroundColor: colors.pinksalmon
+                            }}>
+                            {idx} <br /> {value}
+                        </Link>
+                    </>
+                })}
 
+            </div>
         </div>
 
 
